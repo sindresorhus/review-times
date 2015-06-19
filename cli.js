a020813f497cfe97b90dc658f8a9e7f03badb4c8
Sindre Sorhus <sindresorhus@gmail.com>
2015-06-19 02:42:19 +0200

    simplify cli help creation

--- cli.js
+++ cli.js
@@ -3,15 +3,14 @@
 var meow = require('meow');
 var reviewTimes = require('./');
 
-var cli = meow({
+meow({
 	help: [
 		'Example',
 		'  $ review-times',
 		'  iOS  8 days',
 		'  Mac  6 days'
-	].join('\n')
+	]
 });
-
 
 reviewTimes(function (err, times) {
 	if (err) {
